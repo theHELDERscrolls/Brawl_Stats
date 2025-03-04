46991be1f9614c9b123cb657ed50478bf8b26a10
Helder M. V. R. <manuhelderruiz@gmail.com>
2025-03-04 15:58:56 +0100

    style(css): section content incoming

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -11,5 +11,31 @@ export const Home = () => {
       <h2>Master the arena with your stats!</h2>  
       <h3>Track, compare, and improve your gameplay in every match.</h3>
       ${navbarSocial()}      
+    </section>
+
+    <section id="brawlers_info">
+      <div class="home_container">
+        <div class="section_info">
+          <h2>Know all the brawlers</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore voluptate doloribus id quas nisi, in
+            tenetur, dolore accusamus autem quam repellendus iste veritatis aliquid, quo nesciunt cum? Voluptatum,
+            laborum
+            perspiciatis.</p>
+        </div>
+        <video src="/assets/videos/BS_Banner.mp4" muted autoplay loop></video>
+      </div>
+    </section>
+
+    <section id="players_info">
+      <div class="home_container">
+        <video src="/assets/videos/BS_Banner.mp4" muted autoplay loop></video>
+        <div class="section_info">
+          <h2>Know all the brawlers</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore voluptate doloribus id quas nisi, in
+            tenetur, dolore accusamus autem quam repellendus iste veritatis aliquid, quo nesciunt cum? Voluptatum,
+            laborum
+            perspiciatis.</p>
+        </div>
+      </div>
     </section>`;
 };
