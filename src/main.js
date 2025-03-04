4ca2120bfd2ff0599067a39277c001a028eee3b0
Helder M. V. R. <manuhelderruiz@gmail.com>
2025-03-04 12:39:17 +0100

    style(css): hero ready

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { header } from "./components/Header/header";
+import { Home } from "./pages/Home/home";
 import "./style.css";
 
 const init = () => {
   header();
+  Home();
 };
 
 init();
--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,5 +1,6 @@
 import "./home.css";
-import pageCleaner from "../../utils/pageCleaner";
+import { pageCleaner } from "../../utils/cleanPage";
+import { navbarSocial } from "../../components/Navbar_social/navbarSocial";
 
 export const Home = () => {
   const main = document.querySelector("main");
@@ -8,6 +9,7 @@ export const Home = () => {
     <section id="hero">
       <video src="/assets/videos/BS_Banner.mp4" muted autoplay loop></video>
       <h2>Master the arena with your stats!</h2>  
-      <h3>Track, compare, and improve your gameplay in every match.</h3>      
+      <h3>Track, compare, and improve your gameplay in every match.</h3>
+      ${navbarSocial()}      
     </section>`;
 };
