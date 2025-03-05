b1ab8e06f433dc99ba9c5eb333c3fbf48cc8f495
Helder M. V. R. <manuhelderruiz@gmail.com>
2025-03-05 13:59:52 +0100

    style(css): animations done. ready to media query

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -15,10 +15,3 @@ export const header = () => {
   )}
 `;
 };
-
-//  ${logo("/assets/images/BS_logo.png", "Brawl Stats logo", "Braw Stats")}
-/*   ${anchorDefault(
-    "suggest_btn",
-    "mailto:manuhelderrui@gmail.com?subject=Website suggest",
-    "Suggest"
-  )} */
