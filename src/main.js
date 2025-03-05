83015903e846401d318f893911a78f4113926c36
Helder M. V. R. <manuhelderruiz@gmail.com>
2025-03-05 17:15:58 +0100

    refactor(main.js): home link SPA added

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { header } from "./components/Header/header";
 import { Home } from "./pages/Home/home";
 import "./style.css";
+import { linkPage } from "./utils/linkPage";
 
 const init = () => {
   header();
   Home();
+  linkPage("home_link", Home);
 };
 
 init();
