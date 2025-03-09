c27ae35222ef2297294c9f2600947d3402af986c
Helder M. V. R. <manuhelderruiz@gmail.com>
2025-03-09 10:10:42 +0100

    feat(DOM): divider component created

---
+++ src/components/Divider/divider.js
@@ -0,0 +1,12 @@
+import "./divider.css";
+
+export const createDivider = (classType, imgSrc, imgAlt, h2Href, h2Content) => {
+  const imgSrcAttr =
+    imgSrc && imgAlt ? `<img src="${imgSrc}" alt="${imgAlt}"/>` : "";
+  return `
+  <div class="${classType}_divider">
+    ${imgSrcAttr}
+    <a href="#${h2Href}">${h2Content}</a>
+    <div></div>
+  </div>`;
+};
