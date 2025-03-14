69ba691447cca227b048bdfa0763ecaf5150f183
Helder M. V. R. <manuhelderruiz@gmail.com>
2025-03-14 09:44:12 +0100

    feat: basic map fetch function done. Ready to test

---
+++ src/utils/getMapsInfo.js
@@ -0,0 +1,26 @@
+export const fetchMapsInfo = async () => {
+  try {
+    const res = await fetch("https://api.brawlify.com/v1/maps");
+    const data = await res.json();
+
+    return (data.list || data).map((map) => ({
+      id: map.id,
+      name: map.name,
+      image: map.imageUrl,
+      isDisableb: map.disabled,
+
+      // Game Mode (necessary to classify).
+      scIdGameMode: map.gameMode.scId,
+      nameGameMode: map.gameMode.name, // al filtrar toLowerCase().replace(/\s+/g, "")
+      iconGameMode: map.gameMode.imageUrl,
+    }));
+  } catch (error) {
+    console.error("Error fetching Maps:", error);
+    alert("An error occurred while fetching the data. Please try again later.");
+    return [];
+  }
+};
+
+fetchMapsInfo().then((data) => {
+  console.log(data);
+});
